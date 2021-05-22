a6f951f103ae85cb2533ab43554ecc4ce5de51ff
Webb Pinner <webbpinner@gmail.com>
2021-05-22 08:01:21 -0400

    fixed syntax issue in utils, added export keyword

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 import path from 'path';
-import { API_ROOT_URL, ROOT_PATH IMAGE_PATH } from './client_config';
+import { API_ROOT_URL, ROOT_PATH, IMAGE_PATH } from './client_config';
 
 // This function constructs a URL to an image served by the Sealog server.
 // Normally, this should correspond to the server's IMAGE_ROUTE setting
@@ -9,10 +9,10 @@ import { API_ROOT_URL, ROOT_PATH IMAGE_PATH } from './client_config';
 // caching proxy.
 //
 // Credit rgov (WHOIGit/ndsf-sealog-client)
-function getImageUrl(image_path) {
+export function getImageUrl(image_path) {
   return API_ROOT_URL + IMAGE_PATH + path.basename(image_path);
 }
 
-function handleMissingImage(ev) {
+export function handleMissingImage(ev) {
 	ev.target.src = API_ROOT_URL + ROOT_PATH + 'images/noimage.jpeg';
 }
