7f99ec6668ea5bed03b8c50b5e11cc0072553b1c
Webb Pinner <webbpinner@gmail.com>
2024-08-30 15:25:43 -0400

    Continued polish of UI

--- src/components/cruise_replay.js
+++ src/components/cruise_replay.js
@@ -7,6 +7,7 @@ import Slider, { createSliderWithTooltip } from 'rc-slider'
 import PropTypes from 'prop-types'
 import EventFilterForm from './event_filter_form'
 import AuxDataCards from './aux_data_cards'
+import EventCommentCard from './event_comment_card'
 import EventOptionsCard from './event_options_card'
 import EventCommentModal from './event_comment_modal'
 import CruiseModeDropdown from './cruise_mode_dropdown'
@@ -288,7 +289,7 @@ class CruiseReplay extends Component {
 
       const buttons =
         this.props.event.selected_event.ts && !this.props.event.fetching ? (
-          <span className='w-100 text-center'>
+          <span>
             <FontAwesomeIcon
               className='text-primary'
               key={`start_${this.props.cruise.id}`}
@@ -357,7 +358,6 @@ class CruiseReplay extends Component {
         onChange={() => this.toggleASNAP()}
         disabled={this.props.event.fetching}
         label='Hide ASNAP'
-        className='m-0'
       />
     )
 
@@ -422,7 +422,7 @@ class CruiseReplay extends Component {
             this.props.roles && (this.props.roles.includes('event_logger') || this.props.roles.includes('admin')) ? commentTooltip : null
 
           return (
-            <ListGroup.Item className='event-list-item py-1' key={event.id} active={active}>
+            <ListGroup.Item className='event-list-item' key={event.id} active={active}>
               <span
                 onClick={() => this.handleEventClick(index)}
               >{`${event.ts} <${event.event_author}>: ${event.event_value} ${eventOptions}`}</span>
@@ -436,15 +436,15 @@ class CruiseReplay extends Component {
     }
 
     return this.props.event.fetching ? (
-      <ListGroup.Item className='event-list-item py-1'>Loading...</ListGroup.Item>
+      <ListGroup.Item className='event-list-item'>Loading...</ListGroup.Item>
     ) : (
-      <ListGroup.Item className='event-list-item py-1'>No events found</ListGroup.Item>
+      <ListGroup.Item className='event-list-item'>No events found</ListGroup.Item>
     )
   }
 
   renderEventCard() {
     return (
-      <Card className='border-secondary mt-2'>
+      <Card className='border-secondary'>
         <Card.Header>{this.renderEventListHeader()}</Card.Header>
         <ListGroup
           variant='flush'
@@ -461,7 +461,14 @@ class CruiseReplay extends Component {
   }
 
   render() {
-    const cruise_id = this.props.cruise.cruise_id ? this.props.cruise.cruise_id : 'Loading...'
+    const event_free_text_card = this.props.event.selected_event.event_free_text ? (
+      <Col className='event-data-col' sm={6} md={4} lg={3}>
+        <Card className='event-data-card'>
+          <Card.Header>Free-form Text</Card.Header>
+          <Card.Body>{this.props.event.selected_event.event_free_text}</Card.Body>
+        </Card>
+      </Col>
+    ) : null
 
     const aux_data = this.props.event.selected_event.aux_data
       ? this.props.event.selected_event.aux_data.filter((data) => !excludeAuxDataSources.includes(data.data_source))
@@ -479,28 +486,36 @@ class CruiseReplay extends Component {
               {_Cruises_}
             </span>
             <FontAwesomeIcon icon='chevron-right' fixedWidth />
-            <span className='text-warning'>{cruise_id}</span>
+            <span className='text-warning'>{this.props.cruise.cruise_id || 'Loading...'}</span>
             <FontAwesomeIcon icon='chevron-right' fixedWidth />
             <CruiseModeDropdown onClick={this.handleCruiseModeSelect} active_mode={'Replay'} modes={['Map']} />
           </ButtonToolbar>
         </Row>
         <Row>
           <Col className='px-1 mb-2'>
-            <Card className='event-data-card'>
+            <Card className='event-header-card'>
               <Card.Header>
                 {this.props.event.selected_event.event_value}
-                <span className='float-right'>{this.props.event.selected_event.ts}</span>
+                <span className='float-right'>
+                  {this.props.event.selected_event.event_author} @ {this.props.event.selected_event.ts}
+                </span>
               </Card.Header>
             </Card>
           </Col>
         </Row>
         <Row>
           <AuxDataCards aux_data={aux_data} />
-          <EventOptionsCard event_options={this.props.event.selected_event.event_options || []} />
+          <EventOptionsCard event={this.props.event.selected_event} />
+          {event_free_text_card}
+          <EventCommentCard event={this.props.event.selected_event} />
+        </Row>
+        <Row>
+          <Col className='px-1 mb-2' xl={12}>
+            {this.renderControlsCard()}
+          </Col>
         </Row>
         <Row>
           <Col className='px-1 mb-1' md={9} lg={9}>
-            {this.renderControlsCard()}
             {this.renderEventCard()}
             <CustomPagination
               className='mt-2'
