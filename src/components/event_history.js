08a7f2c3fc879bac686c67213e674191a0c23a17
Webb Pinner <webbpinner@gmail.com>
2025-08-21 15:42:18 -0400

    WIP: #26, add length of event_options to logic

--- src/components/event_history.js
+++ src/components/event_history.js
@@ -378,7 +378,8 @@ class EventHistory extends Component {
             </OverlayTrigger>
           </span>
         </Card.Header>
-        {this.state.showNewEventDetails && (image_data_sources.length || aux_data.length || event_free_text_card) ? (
+        {this.state.showNewEventDetails &&
+        (image_data_sources.length || aux_data.length || event_free_text_card || this.state.event.event_options) ? (
           <Card.Body className='pt-2 pb-1'>
             <Row>
               <ImageryCards image_data_sources={image_data_sources} onClick={this.handleImagePreviewModal} md={4} lg={3} />
