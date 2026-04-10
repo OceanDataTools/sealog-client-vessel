schmidtocean/sealog-client-vehicle

Refactor components: fix bugs, remove dead code, extract shared utilities
Bug fixes:
- event_history: fix race condition causing newest event not to display;
  fix page > 1 case always showing most recent event
- gallery_tab: move paginationTimer to instance variable to avoid stale state
- review_replay: fix stale state reads in replayAdvance, replayReverse,
  handleReviewReplayStart, handleReviewReplayEnd, handleKeyDown;
  move sliderTimer to instance variable; add sliderTimer cleanup on unmount;
  remove empty componentDidUpdate
- event_management: add WebSocket live updates; fix handleEventDelete
  pagination after delete
- execute_modal: add missing componentWillUnmount to disconnect WebSocket

Features:
- event_comment_modal: show image previews for file attachments
- event_template_options_modal: submit eventFileAttachments aux_data record

Refactoring / shared utilities (src/utils.js):
- Extract resolveStartTS, connectWSClient, buildEventQuery
- Adopt these utilities in event_history, event_management,
  event_template_list, footer

Dead code removal:
- Delete unused lowering_dropdown.js
- event_logging: remove empty constructor and unused mapDispatchToProps
- custom_pagination: remove pointless connect(null, null) wrapper
- export_dropdown: eliminate state mirroring props; extract buildQuery helper;
  remove pointless connect(null, null) wrapper
- review_gallery: remove unused replayEventIndex state and dead else branch
- cruise_menu: extract repeated findCurrentCruise() helper

Co-Authored-By: Claude Sonnet 4.6 <noreply@anthropic.com>

src/components/export_dropdown.js
@@ -3,7 +3,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Dropdown, OverlayTrigger, Tooltip } from 'react-bootstrap'
 import PropTypes from 'prop-types'
 import moment from 'moment'
-import { connect } from 'react-redux'
 import {
   get_event_aux_data,
   get_event_aux_data_by_lowering,
@@ -19,82 +18,43 @@ const dateFormat = 'YYYYMMDD'
 const timeFormat = 'HHmm'
 
 class ExportDropdown extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      id: this.props.id ? this.props.id : 'dropdown-download',
-      prefix: this.props.prefix ? this.props.prefix : null,
-      sort: this.props.sort ? this.props.sort : null
+  buildQuery(exportFormat = null) {
+    const eventFilterValue = this.props.eventFilter.value ? this.props.eventFilter.value : this.props.hideASNAP ? '!ASNAP' : null
+    const query = {
+      ...this.props.eventFilter,
+      value: eventFilterValue ? eventFilterValue.split(',') : null,
+      author: this.props.eventFilter.author ? this.props.eventFilter.author.split(',') : null,
+      sort: this.props.sort
     }
-  }
-
-  componentDidUpdate(prevProps) {
-    if (this.props.prefix !== prevProps.prefix) {
-      this.setState({ prefix: this.props.prefix })
+    if (exportFormat) {
+      query.format = exportFormat
+      query.add_record_ids = exportFormat === 'json'
     }
-
-    if (this.props.sort !== prevProps.sort) {
-      this.setState({ sort: this.props.sort })
-    }
+    return query
   }
 
   async fetchEvents(exportFormat) {
-    let eventFilter_value = this.props.eventFilter.value ? this.props.eventFilter.value : this.props.hideASNAP ? '!ASNAP' : null
-
-    const query = {
-      ...this.props.eventFilter,
-      format: exportFormat,
-      add_record_ids: exportFormat === 'json',
-      value: eventFilter_value ? eventFilter_value.split(',') : null,
-      author: this.props.eventFilter.author ? this.props.eventFilter.author.split(',') : null,
-      sort: this.state.sort
-    }
-
-    if (this.props.loweringID) {
-      return await get_events_by_lowering(query, this.props.loweringID)
-    }
+    const query = this.buildQuery(exportFormat)
+    if (this.props.loweringID) return await get_events_by_lowering(query, this.props.loweringID)
     return await get_events(query)
   }
 
   async fetchEventAuxData() {
-    let eventFilter_value = this.props.eventFilter.value ? this.props.eventFilter.value : this.props.hideASNAP ? '!ASNAP' : null
-
-    const query = {
-      ...this.props.eventFilter,
-      value: eventFilter_value ? eventFilter_value.split(',') : null,
-      author: this.props.eventFilter.author ? this.props.eventFilter.author.split(',') : null,
-      sort: this.state.sort
-    }
-
-    if (this.props.loweringID) {
-      return await get_event_aux_data_by_lowering(query, this.props.loweringID)
-    }
+    const query = this.buildQuery()
+    if (this.props.loweringID) return await get_event_aux_data_by_lowering(query, this.props.loweringID)
     return await get_event_aux_data(query)
   }
 
   async fetchEventsWithAuxData(exportFormat) {
-    let eventFilter_value = this.props.eventFilter.value ? this.props.eventFilter.value : this.props.hideASNAP ? '!ASNAP' : null
-
-    const query = {
-      ...this.props.eventFilter,
-      format: exportFormat,
-      add_record_ids: exportFormat === 'json',
-      value: eventFilter_value ? eventFilter_value.split(',') : null,
-      author: this.props.eventFilter.author ? this.props.eventFilter.author.split(',') : null,
-      sort: this.state.sort
-    }
-
-    if (this.props.loweringID) {
-      return await get_event_exports_by_lowering(query, this.props.loweringID)
-    }
+    const query = this.buildQuery(exportFormat)
+    if (this.props.loweringID) return await get_event_exports_by_lowering(query, this.props.loweringID)
     return await get_event_exports(query)
   }
 
   exportEventsWithAuxData(format = 'json') {
     this.fetchEventsWithAuxData(format)
       .then((results) => {
-        const prefix = this.state.prefix ? this.state.prefix : moment.utc(results[0].ts).format(dateFormat + '_' + timeFormat)
+        const prefix = this.props.prefix ? this.props.prefix : moment.utc(results[0].ts).format(dateFormat + '_' + timeFormat)
         fileDownload(format == 'json' ? JSON.stringify(results) : results, `${prefix}_sealog_export.${format}`)
       })
       .catch((error) => {
@@ -105,7 +65,7 @@ class ExportDropdown extends Component {
   exportEvents(format = 'json') {
     this.fetchEvents(format)
       .then((results) => {
-        const prefix = this.state.prefix ? this.state.prefix : moment.utc(results[0].ts).format(dateFormat + '_' + timeFormat)
+        const prefix = this.props.prefix ? this.props.prefix : moment.utc(results[0].ts).format(dateFormat + '_' + timeFormat)
         fileDownload(format == 'json' ? JSON.stringify(results) : results, `${prefix}_sealog_eventExport.${format}`)
       })
       .catch((error) => {
@@ -116,7 +76,7 @@ class ExportDropdown extends Component {
   exportAuxData() {
     this.fetchEventAuxData()
       .then((results) => {
-        const prefix = this.state.prefix ? this.state.prefix : moment.utc(results[0].ts).format(dateFormat + '_' + timeFormat)
+        const prefix = this.props.prefix ? this.props.prefix : moment.utc(results[0].ts).format(dateFormat + '_' + timeFormat)
         fileDownload(JSON.stringify(results), `${prefix}_sealog_auxDataExport.json`)
       })
       .catch((error) => {
@@ -129,7 +89,7 @@ class ExportDropdown extends Component {
     const className = this.props.className ? this.props.className : 'p-0'
 
     return (
-      <Dropdown as={'span'} id={this.state.id}>
+      <Dropdown as={'span'} id={this.props.id || 'dropdown-download'}>
         <Dropdown.Toggle
           className={className}
           style={{ position: 'relative', bottom: '2px' }}
@@ -180,4 +140,4 @@ ExportDropdown.propTypes = {
   className: PropTypes.string
 }
 
-export default connect(null, null)(ExportDropdown)
+export default ExportDropdown

OceanDataTools/sealog-client-vessel

src/components/export_dropdown.js
@@ -3,7 +3,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Dropdown, OverlayTrigger, Tooltip } from 'react-bootstrap'
 import PropTypes from 'prop-types'
 import moment from 'moment'
-import { connect } from 'react-redux'
 import {
   get_event_aux_data,
   get_event_aux_data_by_cruise,
@@ -19,82 +18,43 @@ const dateFormat = 'YYYYMMDD'
 const timeFormat = 'HHmm'
 
 class ExportDropdown extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      id: this.props.id ? this.props.id : 'dropdown-download',
-      prefix: this.props.prefix ? this.props.prefix : null,
-      sort: this.props.sort ? this.props.sort : null
+  buildQuery(exportFormat = null) {
+    const eventFilterValue = this.props.eventFilter.value ? this.props.eventFilter.value : this.props.hideASNAP ? '!ASNAP' : null
+    const query = {
+      ...this.props.eventFilter,
+      value: eventFilterValue ? eventFilterValue.split(',') : null,
+      author: this.props.eventFilter.author ? this.props.eventFilter.author.split(',') : null,
+      sort: this.props.sort
     }
-  }
-
-  componentDidUpdate(prevProps) {
-    if (this.props.prefix !== prevProps.prefix) {
-      this.setState({ prefix: this.props.prefix })
+    if (exportFormat) {
+      query.format = exportFormat
+      query.add_record_ids = exportFormat === 'json'
     }
-
-    if (this.props.sort !== prevProps.sort) {
-      this.setState({ sort: this.props.sort })
-    }
+    return query
   }
 
   async fetchEvents(exportFormat) {
-    let eventFilter_value = this.props.eventFilter.value ? this.props.eventFilter.value : this.props.hideASNAP ? '!ASNAP' : null
-
-    const query = {
-      ...this.props.eventFilter,
-      format: exportFormat,
-      add_record_ids: exportFormat === 'json',
-      value: eventFilter_value ? eventFilter_value.split(',') : null,
-      author: this.props.eventFilter.author ? this.props.eventFilter.author.split(',') : null,
-      sort: this.state.sort
-    }
-
-    if (this.props.cruiseID) {
-      return await get_events_by_cruise(query, this.props.cruiseID)
-    }
+    const query = this.buildQuery(exportFormat)
+    if (this.props.cruiseID) return await get_events_by_cruise(query, this.props.cruiseID)
     return await get_events(query)
   }
 
   async fetchEventAuxData() {
-    let eventFilter_value = this.props.eventFilter.value ? this.props.eventFilter.value : this.props.hideASNAP ? '!ASNAP' : null
-
-    const query = {
-      ...this.props.eventFilter,
-      value: eventFilter_value ? eventFilter_value.split(',') : null,
-      author: this.props.eventFilter.author ? this.props.eventFilter.author.split(',') : null,
-      sort: this.state.sort
-    }
-
-    if (this.props.cruiseID) {
-      return await get_event_aux_data_by_cruise(query, this.props.cruiseID)
-    }
+    const query = this.buildQuery()
+    if (this.props.cruiseID) return await get_event_aux_data_by_cruise(query, this.props.cruiseID)
     return await get_event_aux_data(query)
   }
 
   async fetchEventsWithAuxData(exportFormat) {
-    let eventFilter_value = this.props.eventFilter.value ? this.props.eventFilter.value : this.props.hideASNAP ? '!ASNAP' : null
-
-    const query = {
-      ...this.props.eventFilter,
-      format: exportFormat,
-      add_record_ids: exportFormat === 'json',
-      value: eventFilter_value ? eventFilter_value.split(',') : null,
-      author: this.props.eventFilter.author ? this.props.eventFilter.author.split(',') : null,
-      sort: this.state.sort
-    }
-
-    if (this.props.cruiseID) {
-      return await get_event_exports_by_cruise(query, this.props.cruiseID)
-    }
+    const query = this.buildQuery(exportFormat)
+    if (this.props.cruiseID) return await get_event_exports_by_cruise(query, this.props.cruiseID)
     return await get_event_exports(query)
   }
 
   exportEventsWithAuxData(format = 'json') {
     this.fetchEventsWithAuxData(format)
       .then((results) => {
-        const prefix = this.state.prefix ? this.state.prefix : moment.utc(results[0].ts).format(dateFormat + '_' + timeFormat)
+        const prefix = this.props.prefix ? this.props.prefix : moment.utc(results[0].ts).format(dateFormat + '_' + timeFormat)
         fileDownload(format == 'json' ? JSON.stringify(results) : results, `${prefix}_sealog_export.${format}`)
       })
       .catch((error) => {
@@ -105,7 +65,7 @@ class ExportDropdown extends Component {
   exportEvents(format = 'json') {
     this.fetchEvents(format)
       .then((results) => {
-        const prefix = this.state.prefix ? this.state.prefix : moment.utc(results[0].ts).format(dateFormat + '_' + timeFormat)
+        const prefix = this.props.prefix ? this.props.prefix : moment.utc(results[0].ts).format(dateFormat + '_' + timeFormat)
         fileDownload(format == 'json' ? JSON.stringify(results) : results, `${prefix}_sealog_eventExport.${format}`)
       })
       .catch((error) => {
@@ -116,7 +76,7 @@ class ExportDropdown extends Component {
   exportAuxData() {
     this.fetchEventAuxData()
       .then((results) => {
-        const prefix = this.state.prefix ? this.state.prefix : moment.utc(results[0].ts).format(dateFormat + '_' + timeFormat)
+        const prefix = this.props.prefix ? this.props.prefix : moment.utc(results[0].ts).format(dateFormat + '_' + timeFormat)
         fileDownload(JSON.stringify(results), `${prefix}_sealog_auxDataExport.json`)
       })
       .catch((error) => {
@@ -129,7 +89,7 @@ class ExportDropdown extends Component {
     const className = this.props.className ? this.props.className : 'p-0'
 
     return (
-      <Dropdown as={'span'} id={this.state.id}>
+      <Dropdown as={'span'} id={this.props.id || 'dropdown-download'}>
         <Dropdown.Toggle
           className={className}
           style={{ position: 'relative', bottom: '2px' }}
@@ -180,4 +140,4 @@ ExportDropdown.propTypes = {
   className: PropTypes.string
 }
 
-export default connect(null, null)(ExportDropdown)
+export default ExportDropdown